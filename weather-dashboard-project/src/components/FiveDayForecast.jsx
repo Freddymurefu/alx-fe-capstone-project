f261d446f90c9c26401561977e4fb38f12df8c22
Fredrick Murefu <freddymurefu@gmail.com>
2025-09-01 18:17:22 +0300

    Tweaked FiveDayForecast.jsx to create space for City Image

--- weather-dashboard-project/src/components/FiveDayForecast.jsx
+++ weather-dashboard-project/src/components/FiveDayForecast.jsx
@@ -1,13 +1,13 @@
 
 function FiveDayForecast({ forecast }) {
   return (
-    <div className="bg-white-100 p-6 rounded-2xl shadow-lg max-w-4xl mr-2 mx-auto">
-    <h3 className="text-xl font-semibold text-gray-700 mb-4 text-center">5-day Weather Forecast</h3>
+    <div className="bg-white-100 p-4 sm:p-6 rounded-2xl shadow-lg max-w-5xl ml-0 mr-auto">
+    <h3 className="text-lg sm:text-xl font-semibold text-gray-700 mb-4 text-center">5-day Weather Forecast</h3>
 
-      <ul className="flex flex-wrap justify-between gap-3">
+      <ul className="grid gap-3 grid-cols-2 sm:grid-cols-3 lg:grid-cols-5">
         {forecast.map((day, index) => (
           <li className="flex flex-col items-center bg-gradient-to-br from-blue-50 to-blue-100 
-             p-4 rounded-xl shadow-md hover:shadow-md hover:-translate-y-1 transition-transform duration-300"        
+             p-6 rounded-xl shadow-md hover:shadow-lg hover:-translate-y-1 transition-transform duration-300"        
           key={index} style={{ textAlign: "center" }}>
             <p className="font-medium">{day.day}</p>
             <img
