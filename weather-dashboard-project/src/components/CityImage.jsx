77d65fd16ac7a0e2b8a746cfe0892570b47be6b2
Fredrick Murefu <freddymurefu@gmail.com>
2025-09-01 19:41:54 +0300

    Added Function to Normalise Descriptions to Match Images

--- weather-dashboard-project/src/components/CityImage.jsx
+++ weather-dashboard-project/src/components/CityImage.jsx
@@ -6,6 +6,18 @@ import snowImg from "../assets/weatherImages/snow.jpg";
 import thunderstormImg from "../assets/weatherImages/thunderstorm.jpg";
 import sunnyImg from "../assets/weatherImages/sunny.jpg";
 
+function mapDescriptionToCondition(description) {     //Function normalises description to match images
+  const desc = description.toLowerCase();
+
+  if (desc.includes("cloud")) return "Cloudy";
+  if (desc.includes("rain")) return "Rain";
+  if (desc.includes("snow")) return "Snow";
+  if (desc.includes("thunderstorm")) return "Thunderstorm";
+  if (desc.includes("mist") || desc.includes("fog") || desc.includes("haze")) return "Mist";
+  if (desc.includes("clear")) return "Clear";
+
+  return "Sunny"; //The fallbackn option
+}
 
 
 const images = {
